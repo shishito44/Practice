c1edb93254beee20b03a2a4920f5c5b3f5b5f2f4
DESKTOP-NKURHCG\user <shunshun6640@outlook.jp>
2025-08-05 13:48:19 +0900

    Geminetを利用した生成AIGeminiが直接使えるプログラム

---
+++ Gemini api/Gemini api/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GemiNet;
+using Microsoft.Extensions.Configuration;
+
+IConfiguration configuration = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+string message = configuration.GetSection("Secrets:Message").Value ?? "Failed";
+
+using var ai = new GoogleGenAI
+{
+    ApiKey = message
+};
+
+var response = await ai.Models.GenerateContentAsync(new()
+{
+    Model = Models.Gemini2_0Flash,    // models/gemini-2.0-flash
+    Contents = "こんにちは"
+});
+
+Console.WriteLine(response.GetText());
+
+
+
