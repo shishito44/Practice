ec403d77b6d0af49fc76923ca1498325e023eaa6
DESKTOP-NKURHCG\user <shunshun6640@outlook.jp>
2025-08-07 09:26:19 +0900

    架空の会話内容を作り議事録にするプログラム修正中

--- GeminiApi/GeminiApi2/Program.cs
+++ GeminiApi/GeminiApi2/Program.cs
@@ -18,13 +18,11 @@ string meetingplompt = "C:/repo/Practice/textfile/meetingplompt.txt";
 string minutesplompt = "C:/repo/Practice/textfile/minutesplompt.txt";
 string filePath = "C:/repo/Practice/textfile/newfile.txt";
 string? meetingtext = null;
-string? menutestext = null;
+string? minutestext = null;
 
-using (StreamReader sr = new StreamReader(meetingplompt))
-{
-    meetingtext = sr.ReadToEnd();
-    Console.WriteLine(meetingtext);
-}
+meetingtext = System.IO.File.ReadAllText(meetingplompt);
+Console.WriteLine(meetingtext);
+
 var response1 = await ai.Models.GenerateContentAsync(new()
 {
     Model = Models.Gemini2_0Flash,    // models/gemini-2.0-flash
@@ -37,17 +35,14 @@ using (StreamWriter writer = new StreamWriter(filePath,false))
     writer.WriteLine(minutesfile);
 }
 Console.WriteLine(minutesfile);
-using (StreamReader sr = new StreamReader(minutesplompt))
-{
-    menutestext = sr.ReadToEnd();
-    Console.WriteLine(menutestext);
-}
 
+minutestext = System.IO.File.ReadAllText(minutesplompt);
+Console.WriteLine(minutestext);
 
 var response2 = await ai.Models.GenerateContentAsync(new()
 {
     Model = Models.Gemini2_0Flash,    // models/gemini-2.0-flash
-    Contents = minutesfile + "\n" + minutesplompt
+    Contents = minutesfile + "\n" + minutestext
 });
 
 Console.WriteLine(response2.GetText());
